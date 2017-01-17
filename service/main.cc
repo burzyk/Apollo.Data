2747cefb3bad41b34436b16c52097cb75652c6ae
burzyk <p.k.burzynski@gmail.com>
2017-01-17 23:58:57 +0000

    added main

--- service/main.cc
+++ service/main.cc
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <engine/storage/storage.h>
+#include <engine/storage/cached-storage.h>
+#include <engine/database.h>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  // storage_allocate_page_test();
+  // return 0;
+
+  // Storage *storage = MmapFileStorage::Init("/Users/pburzynski/apollo-test/data/DATA_FILE", A_DATA_CHUNK_SIZE);
+  apollo::Storage *storage = apollo::CachedStorage::Init("/Users/pburzynski/apollo-test/data/DATA_FILE", 1000);
+  apollo::Database *db = apollo::Database::Init(storage);
+  std::cout << "db initialized" << std::endl;
+
+  int batches = 10;
+  int batch_size = 10;
+  uint64_t time = 1;
+  apollo::data_point_t *points = new apollo::data_point_t[batch_size];
+
+  struct timespec start = {0};
+  clock_gettime(CLOCK_REALTIME, &start);
+
+  for (int i = 0; i < batches; i++) {
+    for (int j = 0; j < batch_size; j++) {
+      points[j].time = time++;
+      points[j].value = time * 100;
+    }
+
+    db->Write("usd_gbp", points, batch_size);
+  }
+
+
+  struct timespec stop = {0};
+  clock_gettime(CLOCK_REALTIME, &stop);
+  printf("%lu.%lu\n", stop.tv_sec - start.tv_sec, stop.tv_nsec - start.tv_nsec);
+
+  db->PrintMetadata();
+
+  delete db;
+  delete storage;
+
   return 0;
 }
