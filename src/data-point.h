d58086c52b64a850259fd6bbba0d98a01fc95c53
burzyk <p.k.burzynski@gmail.com>
2017-02-26 22:31:35 +0000

    #41 compressed data_point_t

--- src/data-point.h
+++ src/data-point.h
@@ -11,13 +11,13 @@ namespace shakadb {
 
 typedef uint64_t timestamp_t;
 
-struct data_point_t {
+struct data_point_t  {
   static const timestamp_t kMaxTimestamp;
   static const timestamp_t kMinTimestamp;
 
   timestamp_t time;
   float value;
-};
+} __attribute__((packed));
 
 }
 
