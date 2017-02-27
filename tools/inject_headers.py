3ff407d1f317517706026b082bd5ae6250c46080
burzyk <p.k.burzynski@gmail.com>
2017-02-27 23:51:03 +0000

    #34 added tool to include license in all files

---
+++ tools/inject_headers.py
@@ -0,0 +1,30 @@
+import glob
+import sys
+import itertools as it
+
+
+def process_files(header, pattern):
+    for file_name in glob.glob(pattern, recursive=True):
+        print('processing file: ' + file_name)
+        inject_header(header, file_name)
+
+
+def inject_header(header, file_name):
+    with open(file_name) as f:
+        no_header_lines = list(it.dropwhile(lambda l: len(l) > 1 and l[1] == '*', f.readlines()))
+        new_content = header + no_header_lines
+
+    with open(file_name, mode='w') as f:
+        f.writelines(new_content)
+
+
+if __name__ == "__main__":
+    root = sys.argv[1] if len(sys.argv) > 1 else '..'
+    with open(root + '/LICENSE') as lic:
+        header = ['/*\n'] + [' * ' + l for l in lic.readlines()] + [' */\n']
+        extensions = ['h', 'cc', 'c']
+        paths = ['src', 'test']
+
+        for ext in extensions:
+            for path in paths:
+                process_files(header, root + '/{0}/**/*.{1}'.format(path, ext))
