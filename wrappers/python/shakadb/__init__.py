0fe504890332cbc38ed16cb50f6ca0172db51d38
burzyk <p.k.burzynski@gmail.com>
2017-05-08 22:36:42 +0100

    #71 added package meta information

--- wrappers/python/shakadb/__init__.py
+++ wrappers/python/shakadb/__init__.py
@@ -1,2 +1,9 @@
 from .shakadb import Session, DataPointsIterator
 from .common import ShakaDbError, Constants
+
+__title__ = 'shakadb'
+__version__ = '0.0.1'
+__build__ = 0x021300
+__author__ = 'burzyk'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 burzyk'
