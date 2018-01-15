8d36c13ce2e3bb1925e7cdee697d0040578f9a95
burzyk <p.k.burzynski@gmail.com>
2018-01-15 22:37:13 +0000

    #107 read response and read latest request

--- clients/dotnet/ShakaDB.Client.Tests/ProtocolTests.cs
+++ clients/dotnet/ShakaDB.Client.Tests/ProtocolTests.cs
@@ -47,5 +47,44 @@
             Assert.Equal(100L, (long) deserialized.End);
             Assert.Equal(123, deserialized.PointsPerPacket);
         }
+
+        [Fact]
+        public void SerializeReadLatestRequestTest()
+        {
+            var request = new ReadLatestRequest(76);
+
+            var serialized = request.Serialize();
+
+            Assert.Equal(5, serialized.Length);
+
+            var deserialized = new ReadLatestRequest(serialized);
+
+            Assert.Equal(PacketType.ReadLatestRequest, deserialized.PacketType);
+            Assert.Equal(76, (int) deserialized.DataSeriesId);
+        }
+
+        [Fact]
+        public void SerializeReadResponseTest()
+        {
+            var points = new[]
+            {
+                new DataPoint(1, 2.4f),
+                new DataPoint(1, 5.4f)
+            };
+            var request = new ReadResponse(points);
+
+            var serialized = request.Serialize();
+
+            Assert.Equal(29, serialized.Length);
+
+            var deserialized = new ReadResponse(serialized);
+
+            Assert.Equal(PacketType.ReadResponse, deserialized.PacketType);
+            Assert.Equal(2, deserialized.Points.Count);
+            Assert.Equal(1, (int) deserialized.Points[0].Timestamp);
+            Assert.Equal(2.4f, deserialized.Points[0].Value);
+            Assert.Equal(1, (int) deserialized.Points[1].Timestamp);
+            Assert.Equal(5.4f, deserialized.Points[1].Value);
+        }
     }
 }
---
+++ clients/dotnet/ShakaDB.Client/Protocol/ReadLatestRequest.cs
@@ -0,0 +1,30 @@
+﻿namespace ShakaDB.Client.Protocol
+{
+    using System.IO;
+
+    public class ReadLatestRequest : BasePacket
+    {
+        public ReadLatestRequest(uint dataSeriesId)
+            : base(PacketType.ReadLatestRequest)
+        {
+            DataSeriesId = dataSeriesId;
+        }
+
+        public ReadLatestRequest(byte[] payload)
+            : base(payload)
+        {
+        }
+
+        public uint DataSeriesId { get; private set; }
+
+        protected override void Load(BinaryReader reader)
+        {
+            DataSeriesId = reader.ReadUInt32();
+        }
+
+        protected override void Save(BinaryWriter writer)
+        {
+            writer.Write(DataSeriesId);
+        }
+    }
+}
