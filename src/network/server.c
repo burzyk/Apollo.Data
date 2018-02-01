1b1d23474e8909f33183ca509bc9f660fbde46db
burzyk <p.k.burzynski@gmail.com>
2018-02-01 09:42:30 +0000

    fixed memory access issue in libuv

--- src/network/server.c
+++ src/network/server.c
@@ -18,6 +18,7 @@ void on_client_connected(uv_stream_t *master_socket, int status);
 void on_data_read(uv_stream_t *client_socket, ssize_t nread, const uv_buf_t *buf);
 void on_write_complete(uv_write_t *req, int status);
 void on_server_shutdown(uv_async_t *request);
+void on_client_free(uv_handle_t *socket);
 
 client_t *client_create(server_t *server, int index) {
   client_t *client = sdb_alloc(sizeof(client_t));
@@ -38,27 +39,30 @@ client_t *client_create(server_t *server, int index) {
 
 void client_disconnect_and_destroy(client_t *client) {
   client->server->clients[client->index] = NULL;
-  uv_close((uv_handle_t *) &client->socket, NULL);
-  sdb_free(client);
+  uv_close((uv_handle_t *)&client->socket, on_client_free);
+}
+
+void on_client_free(uv_handle_t *socket) {
+  sdb_free(socket->data);
 }
 
 int client_send_and_destroy_data(client_t *client, uint8_t *data, size_t count) {
-  uv_write_t *request = (uv_write_t *) sdb_alloc(sizeof(uv_write_t));
-  uv_buf_t *buf = (uv_buf_t *) sdb_alloc(sizeof(uv_buf_t) * 2);
+  uv_write_t *request = (uv_write_t *)sdb_alloc(sizeof(uv_write_t));
+  uv_buf_t *buf = (uv_buf_t *)sdb_alloc(sizeof(uv_buf_t) * 2);
   request->data = buf;
 
-  packet_t *packet = (packet_t *) sdb_alloc(sizeof(packet_t));
+  packet_t *packet = (packet_t *)sdb_alloc(sizeof(packet_t));
   packet->magic = SDB_SERVER_MAGIC;
   packet->total_size = sizeof(packet_t) + count;
 
-  buf[0].base = (char *) packet;
+  buf[0].base = (char *)packet;
   buf[0].len = sizeof(packet_t);
-  buf[1].base = (char *) data;
+  buf[1].base = (char *)data;
   buf[1].len = count;
 
   int status;
 
-  if ((status = uv_write(request, (uv_stream_t *) &client->socket, buf, 2, on_write_complete)) >= 0) {
+  if ((status = uv_write(request, (uv_stream_t *)&client->socket, buf, 2, on_write_complete)) >= 0) {
     return 0;
   }
 
@@ -68,7 +72,7 @@ int client_send_and_destroy_data(client_t *client, uint8_t *data, size_t count) 
 }
 
 void on_write_complete(uv_write_t *req, int status) {
-  uv_buf_t *buf = (uv_buf_t *) req->data;
+  uv_buf_t *buf = (uv_buf_t *)req->data;
 
   sdb_free(buf[0].base);
   sdb_free(buf[1].base);
@@ -100,11 +104,11 @@ void server_run(server_t *server) {
   struct sockaddr_in address = {0};
   uv_ip4_addr("0.0.0.0", server->port, &address);
 
-  if (uv_tcp_bind(&server->master_socket, (const struct sockaddr *) &address, 0) < 0) {
+  if (uv_tcp_bind(&server->master_socket, (const struct sockaddr *)&address, 0) < 0) {
     die("Failed to bind to the master socket");
   }
 
-  if (uv_listen((uv_stream_t *) &server->master_socket, 10, on_client_connected) != 0) {
+  if (uv_listen((uv_stream_t *)&server->master_socket, 10, on_client_connected) != 0) {
     die("Failed to listen for incoming clients");
   }
 
@@ -119,14 +123,14 @@ void server_stop(server_t *server) {
 }
 
 void on_server_shutdown(uv_async_t *request) {
-  server_t *server = (server_t *) request->data;
+  server_t *server = (server_t *)request->data;
 
   if (server == NULL) {
     return;
   }
 
   log_info("Received shutdown request");
-  uv_close((uv_handle_t *) &server->master_socket, NULL);
+  uv_close((uv_handle_t *)&server->master_socket, NULL);
 
   for (int i = 0; i < SDB_MAX_CLIENTS; i++) {
     client_t *client = server->clients[i];
@@ -137,18 +141,18 @@ void on_server_shutdown(uv_async_t *request) {
   }
 
   log_info("All clients disconnected");
-  uv_close((uv_handle_t *) request, NULL);
+  uv_close((uv_handle_t *)request, NULL);
 }
 
 void on_alloc(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
-  suggested_size = (size_t) sdb_min(SDB_SERVER_READ_BUFFER_MAX_LEN, (int) suggested_size);
+  suggested_size = (size_t)sdb_min(SDB_SERVER_READ_BUFFER_MAX_LEN, (int)suggested_size);
 
   buf->base = sdb_alloc(suggested_size);
   buf->len = suggested_size;
 }
 
 void on_client_connected(uv_stream_t *master_socket, int status) {
-  server_t *server = (server_t *) master_socket->data;
+  server_t *server = (server_t *)master_socket->data;
 
   if (status < 0) {
     log_error("New connection error: %s", uv_strerror(status));
@@ -156,32 +160,32 @@ void on_client_connected(uv_stream_t *master_socket, int status) {
   }
 
   log_debug("Client connected");
-  client_t *client = NULL;
+  int new_client_index = 0;
 
-  for (int i = 0; i < SDB_MAX_CLIENTS && client == NULL; i++) {
-    if (server->clients[i] == NULL) {
-      client = server->clients[i] = client_create(server, i);
-    }
+  while (new_client_index < SDB_MAX_CLIENTS && server->clients[new_client_index] == NULL) {
+    new_client_index++;
   }
 
-  if (client == NULL) {
+  if (new_client_index >= SDB_MAX_CLIENTS) {
     log_debug("Max clients connected, ignoring this client");
     return;
   }
 
-  if ((status = uv_accept((uv_stream_t *) &server->master_socket, (uv_stream_t *) &client->socket)) < 0) {
+  client_t *client = server->clients[new_client_index] = client_create(server, new_client_index);
+
+  if ((status = uv_accept((uv_stream_t *)&server->master_socket, (uv_stream_t *)&client->socket)) < 0) {
     log_error("Failed to accept the connection: %s", uv_strerror(status));
     client_disconnect_and_destroy(client);
   }
 
-  if ((status = uv_read_start((uv_stream_t *) client, on_alloc, on_data_read)) < 0) {
+  if ((status = uv_read_start((uv_stream_t *)client, on_alloc, on_data_read)) < 0) {
     log_error("Failed to start reading: %s", uv_strerror(status));
     client_disconnect_and_destroy(client);
   }
 }
 
 void on_data_read(uv_stream_t *client_socket, ssize_t nread, const uv_buf_t *buf) {
-  client_t *client = (client_t *) client_socket->data;
+  client_t *client = (client_t *)client_socket->data;
 
   if (nread < 0) {
     log_debug("Client disconnected, %d", client->index);
@@ -192,7 +196,7 @@ void on_data_read(uv_stream_t *client_socket, ssize_t nread, const uv_buf_t *buf
 
     client_disconnect_and_destroy(client);
   } else if (nread > 0) {
-    memcpy(client->buffer + client->buffer_length, buf->base, (size_t) nread);
+    memcpy(client->buffer + client->buffer_length, buf->base, (size_t)nread);
     client->buffer_length += nread;
     sdb_free(buf->base);
 
@@ -200,7 +204,7 @@ void on_data_read(uv_stream_t *client_socket, ssize_t nread, const uv_buf_t *buf
       return;
     }
 
-    packet_t *packet = (packet_t *) client->buffer;
+    packet_t *packet = (packet_t *)client->buffer;
 
     if (packet->magic != SDB_SERVER_MAGIC || packet->total_size > SDB_SERVER_PACKET_MAX_LEN) {
       log_error("Received malformed packet, disconnecting, magic: %u, len: %u", packet->magic, packet->total_size);
