d3daebe70e8995129753ce62afab92076d15e023
burzyk <p.k.burzynski@gmail.com>
2018-02-05 23:46:08 +0000

    removed old python wrapper

--- clients/python/tests/api_tests.py
+++ clients/python/tests/api_tests.py
@@ -24,25 +24,12 @@ def test_multiple_open_iterators():
         s.read(0, 100, 1000)
 
 
-def test_reuse_closed_session():
-    with raises(pyshaka.SessionClosedError):
-        s = pyshaka.Session('localhost', 8487)
-        s.close()
-        s.read_all(0, 0, 100)
-
-
 def test_server_read_and_write_with_iterator():
     with pyshaka.Session('localhost', 8487) as session:
         session.truncate(TestConstants.USD_AUD)
         session.write(TestConstants.USD_AUD, [(1, 12), (2, 13)])
-        it = session.read(
-            TestConstants.USD_AUD,
-            pyshaka.Constants.SHAKADB_MIN_TIMESTAMP,
-            pyshaka.Constants.SHAKADB_MAX_TIMESTAMP)
-        points = []
-
-        while it.next() != 0:
-            points = points + it.points()
+        it = session.read(TestConstants.USD_AUD, 0, 10)
+        points = list(it)
 
         assert len(points) == 2
         assert points[0][0] == 1
@@ -55,17 +42,10 @@ def test_server_read_and_write_with_iterator_and_points_per_packet_limit():
     with pyshaka.Session('localhost', 8487) as session:
         session.truncate(TestConstants.USD_AUD)
         session.write(TestConstants.USD_AUD, [(1, 12), (2, 13), (3, 14)])
-        it = session.read(
-            TestConstants.USD_AUD,
-            pyshaka.Constants.SHAKADB_MIN_TIMESTAMP,
-            pyshaka.Constants.SHAKADB_MAX_TIMESTAMP,
-            2)
+        it = session.read(TestConstants.USD_AUD, 0, 10, 2)
+        points = list(it)
 
-        assert it.next() != 0
-        assert len(it.points()) == 2
-        assert it.next() != 0
-        assert len(it.points()) == 1
-        assert it.next() == 0
+        assert len(points) == 3
 
 
 def test_server_read_and_write_with_iterator_and_limit():
@@ -73,30 +53,11 @@ def test_server_read_and_write_with_iterator_and_limit():
         session.truncate(TestConstants.USD_AUD)
         session.write(TestConstants.USD_AUD, [(1, 12), (2, 13)])
         it = session.read(TestConstants.USD_AUD, 0, 2)
-        points = []
-
-        while it.next() != 0:
-            points = points + it.points()
+        points = list(it)
 
         assert len(points) == 1
         assert points[0][0] == 1
         assert points[0][1] == 12
-
-
-def test_server_read_and_write():
-    with pyshaka.Session('localhost', 8487) as session:
-        session.truncate(TestConstants.USD_AUD)
-        session.write(TestConstants.USD_AUD, [(1, 12), (2, 13)])
-        points = session.read_all(
-            TestConstants.USD_AUD,
-            pyshaka.Constants.SHAKADB_MIN_TIMESTAMP,
-            pyshaka.Constants.SHAKADB_MAX_TIMESTAMP)
-
-        assert len(points) == 2
-        assert points[0][0] == 1
-        assert points[1][0] == 2
-        assert points[0][1] == 12
-        assert points[1][1] == 13
 
 
 def test_read_latest_no_data():
@@ -120,7 +81,7 @@ def test_server_read_and_write_and_limit():
     with pyshaka.Session('localhost', 8487) as session:
         session.truncate(TestConstants.USD_AUD)
         session.write(TestConstants.USD_AUD, [(1, 12), (2, 13)])
-        points = session.read_all(TestConstants.USD_AUD, 0, 2)
+        points = list(session.read(TestConstants.USD_AUD, 0, 2))
 
         assert len(points) == 1
         assert points[0][0] == 1
@@ -131,14 +92,14 @@ def test_server_truncate_test():
     with pyshaka.Session('localhost', 8487) as session:
         session.truncate(TestConstants.USD_AUD)
         session.write(TestConstants.USD_AUD, [(1, 12), (2, 13)])
-        points = session.read_all(TestConstants.USD_AUD, 0, 2)
+        points = list(session.read(TestConstants.USD_AUD, 0, 2))
 
         assert len(points) == 1
         assert points[0][0] == 1
         assert points[0][1] == 12
 
         session.truncate(TestConstants.USD_AUD)
-        points = session.read_all(TestConstants.USD_AUD, 0, 2)
+        points = list(session.read(TestConstants.USD_AUD, 0, 2))
 
         assert len(points) == 0
 
